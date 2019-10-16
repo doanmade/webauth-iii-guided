d0c8066cac254aee68d4137309a7b189116f7f64
doanmade <43007082+doanmade@users.noreply.github.com>
2019-10-16 09:50:07 -0700

    Login done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-
+const jwt = require("jsonwebtoken")
 const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
@@ -25,8 +25,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = genrateToken(user)
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token,
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +38,17 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function genrateToken(user){
+const payload = {
+  username: user.username,
+  subject: user.id
+}
+const secret ='its a secret! dont tell anyone'
+const options = {
+  expiresIn: '2h'
+}
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
